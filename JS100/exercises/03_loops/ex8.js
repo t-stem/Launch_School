9c1002180def3b0ed01161538c4e08cd1a4b00b7
Tim <N/A>
2025-11-20 12:04:43 +0000

    fixed != comment again

--- JS100/exercises/03_loops/ex8.js
+++ JS100/exercises/03_loops/ex8.js
@@ -3,7 +3,7 @@
 let i = 1;
 
 while (i <= 40) { 
-    if (i % 2 !== 0) {console.log(i)} // use !== instead of <= for strict inequality!
+    if (i % 2 !== 0) {console.log(i)} // use !== instead of != for strict inequality!
     i += 1;
 }
 
