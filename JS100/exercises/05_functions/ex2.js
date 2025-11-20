2eaad29491fed402857458beb617341d54e7dfbb
Tim <N/A>
2025-11-20 13:31:59 +0000

    submitting functions ex2

---
+++ JS100/exercises/05_functions/ex2.js
@@ -0,0 +1,8 @@
+/* Write a function that logs Brendan Eich's quote 'Always bet on JavaScript.' to the console. 
+What is the return value of the function? */
+
+function brendanEichQuote() {
+    console.log('Always bet on JavaScript.');
+}
+
+brendanEichQuote()
