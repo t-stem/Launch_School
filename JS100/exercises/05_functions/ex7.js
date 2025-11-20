8b23853acbedd9f70c0eebb5dd8a9e540e93d90c
Tim <N/A>
2025-11-20 14:02:55 +0000

    submitting functions ex7

---
+++ JS100/exercises/05_functions/ex7.js
@@ -0,0 +1,6 @@
+/* Use JavaScript's string methods on the string 
+'Captain Ruby' to produce the string 'Captain JavaScript'. */
+
+let string = 'Captain Ruby';
+
+console.log(string.replace("Ruby", "JavaScript"));
