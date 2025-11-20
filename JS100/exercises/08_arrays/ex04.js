13f6cfec3253d1631a77c7c42286a39ecc082500
Tim <N/A>
2025-11-20 20:37:56 +0000

    submitting ex04 for arrays

---
+++ JS100/exercises/08_arrays/ex04.js
@@ -0,0 +1,7 @@
+/* Split the string alphabet into an array of characters. */
+
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+let alphabetArray = alphabet.split("");
+
+console.log(alphabetArray);
