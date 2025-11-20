88755b2afb5fa1e759e8a70554c24612d6b45b40
Tim <N/A>
2025-11-20 11:19:30 +0000

    submitting loops ex5

---
+++ JS100/exercises/03_loops/ex5.js
@@ -0,0 +1,10 @@
+/* Using the code below as a starting point, write a while loop that logs the elements of array at each index,
+ and terminates after logging the last element of the array. */
+
+let array = [1, 2, 3, 4];
+let index = 0;
+
+for (i = index; i < array.length; i += 1) {
+    console.log(array[i]);
+}
+
