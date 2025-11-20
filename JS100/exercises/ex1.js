68be019fc6afd17ea878f1803d20479df0167df2
Tim <N/A>
2025-11-20 10:51:47 +0000

    committing deletions staged using git add -A

--- JS100/exercises/ex1.js
+++
@@ -1,9 +0,0 @@
-let iceCreamTaste = 'chocolate';
-let iceCreamDensity = 10;
-
-while (iceCreamDensity > 0) { // correction: moved opening curly brace to this line
-    console.log('Drip...');
-    iceCreamDensity -= 1;
-}
-
-console.log('The ' + iceCreamTaste + ' ice cream melted.'); // correction: added spaces around + sign
--- JS100/exercises/reading_documentatation_2/ex8.js
+++
@@ -1,6 +0,0 @@
-let str1 = "Hello";
-let str2 = "World";
-
-console.log(str1 + " " + str2);
-console.log(str1.concat(" ", str2));
-console.log(`${str1} ${str2}`);
