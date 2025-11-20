2ee1ad120ce00bd04cd6a5177ec52c2c753e447e
Tim <N/A>
2025-11-20 11:59:41 +0000

    fixed strict inequality !==, and added alternative solution

--- JS100/exercises/03_loops/ex8.js
+++ JS100/exercises/03_loops/ex8.js
@@ -1,11 +1,19 @@
 /* Write a while loop that logs all odd natural numbers between 1 and 40. */
 
 let i = 1;
-let sum = 0;
 
-while (i <= 40) {
-    if (i % 2 != 0) {sum = sum + i}
+while (i <= 40) { 
+    if (i % 2 !== 0) {console.log(i)} // use <== instead of <= for strict inequality!
     i += 1;
 }
 
-console.log(sum)
+// alternative solution
+
+console.log("Alternative solution: ")
+
+i = 1;
+
+while (i < 40) {
+    console.log(i);
+    i += 2;
+}
