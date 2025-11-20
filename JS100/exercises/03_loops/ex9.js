332d6e278c759ce38d158556e0c974e8c5cfcc56
Tim <N/A>
2025-11-20 12:11:50 +0000

    added fix to stop loop after logging 'nemo'

--- JS100/exercises/03_loops/ex9.js
+++ JS100/exercises/03_loops/ex9.js
@@ -1,8 +1,10 @@
-/* Loop over the elements of the array fish, logging each one. Terminate the loop immediately after logging the string 'Nemo'.*/
+/* Loop over the elements of the array fish, logging each one. 
+Terminate the loop immediately after logging the string 'Nemo'.*/
 
 let fish = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce'];
 
 for (let i = 0; i < fish.length; i += 1) {
     console.log(fish[i]);
+    if (fish[i] === 'Nemo') {break}
 }
 
