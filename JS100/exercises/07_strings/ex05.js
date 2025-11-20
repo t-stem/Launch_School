0f0de49b2c2844d86edd56b17cf8b042906ab622
Tim <N/A>
2025-11-20 17:42:02 +0000

    submitting ex05 for strings

---
+++ JS100/exercises/07_strings/ex05.js
@@ -0,0 +1,13 @@
+/* Given strings like the following, how can you check whether they're
+ equal irrespective of whether the characters they contain are upper or lower case? 
+ */
+
+let string1 = 'Polar Bear';
+let string2 = 'Polar bear';
+let string3 = 'Penguin';
+
+console.log(string1.toLowerCase() === string2.toLowerCase() === string3.toLowerCase());
+
+
+
+
