5b029cc0102a65636ab9798816abf1de0e8ea456
Tim <N/A>
2025-11-20 17:37:01 +0000

    added alternative method with template literal

--- JS100/exercises/07_strings/ex04.js
+++ JS100/exercises/07_strings/ex04.js
@@ -1,5 +1,10 @@
 /* How can you assign this string to a single variable, preserving the line break? */
 
-const string = "A pirate I was meant to be!\nTrim the sails and roam the sea!"
+const rhyme = "A pirate I was meant to be!\nTrim the sails and roam the sea!"
 
-console.log(string);
+console.log(rhyme);
+
+const message = `A pirate I was meant to be!
+Trim the sails and roam the sea!`
+
+console.log(message);
