6377d30c130dfb950fde26cc6fba815a7b430a11
Tim <N/A>
2025-11-20 12:36:17 +0000

    submitting ex3

--- JS100/exercises/04_conditionals/ex3.js
+++ JS100/exercises/04_conditionals/ex3.js
@@ -1,7 +1,5 @@
-/* The code provided below will randomly initialize randomNumber to either 0 or 1 
-each time it is executed.
-
-Write an if statement that logs 'Yes!' if randomNumber is 1, and 'No.' if randomNumber is 0. */
+/* Take your code from the previous exercise 
+and rewrite the conditional so that it uses the ternary if-then-else operator. */
 
 let randomNumber = Math.round(Math.random());
 
