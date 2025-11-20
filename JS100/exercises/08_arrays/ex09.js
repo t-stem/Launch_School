c5248fbcb8b8fe3d20f9f8071c6bcbe25732fbf3
Tim <N/A>
2025-11-20 21:36:36 +0000

    submitting arrays ex09

---
+++ JS100/exercises/08_arrays/ex09.js
@@ -0,0 +1,19 @@
+/* Write a function that checks whether or not a particular destination is included within destinations,
+without using the built-in method Array.prototype.includes().
+
+For example: When checking whether 'Barcelona' is contained in destinations, 
+the expected output is true, whereas the expected output for 'Nashville' is false.
+*/
+
+let destinations = ['Prague', 'London', 'Sydney', 'Belfast', 'Rome',
+  'Aruba', 'Paris', 'Bora Bora', 'Barcelona', 'Rio de Janeiro',
+  'Marrakesh', 'New York City'];
+
+const contains = function(city, destinations) {
+    if (destinations.indexOf(city) === -1) {return false}
+    else {return true}
+}
+
+
+console.log(contains('Barcelona', destinations)); // true
+console.log(contains('Nashville', destinations)); // false
