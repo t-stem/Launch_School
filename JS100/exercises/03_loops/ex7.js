c0bcab809bd9306e018d82b9c657690e39320642
Tim <N/A>
2025-11-20 11:42:06 +0000

    submitting loops ex7

---
+++ JS100/exercises/03_loops/ex7.js
@@ -0,0 +1,9 @@
+/* The following code keeps looping forever. (You can hit Ctrl-C to stop it.) 
+Why is that? Also modify it so that it stops after the first iteration. */
+
+// it keeps looping because it has no condition for termination between the ; ;. A
+// adding a condition that will evaluate as truthy here will terminate the loop
+
+for (let i = 0; i < 1; i += 1) {
+  console.log("and on");
+}
