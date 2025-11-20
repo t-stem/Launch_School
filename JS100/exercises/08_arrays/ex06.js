508e0fc54933556857d07ca6e125777dd3d88d55
Tim <N/A>
2025-11-20 20:53:18 +0000

    submitting ex06 for arrays

---
+++ JS100/exercises/08_arrays/ex06.js
@@ -0,0 +1,18 @@
+/* We've been given an array of vocabulary words grouped into sub-arrays by meaning.
+This is a two-dimensional array or a nested array.
+ Write some code that iterates through the sub-arrays and logs each vocabulary word to the console. */
+
+ let vocabulary = [
+  ['happy', 'cheerful', 'merry', 'glad'],
+  ['tired', 'sleepy', 'fatigued', 'drained'],
+  ['excited', 'eager', 'enthused', 'animated']
+];
+
+vocabulary.forEach(subArray => subArray.forEach(element => console.log(element)));
+
+
+// Expected output:
+// happy
+// cheerful
+// merry
+// etc...
