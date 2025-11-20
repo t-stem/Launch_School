e8863422911f77c65824930e8b1a69e9bf18bcc0
Tim <N/A>
2025-11-20 13:43:32 +0000

    submitting functions1 ex4

---
+++ JS100/exercises/05_functions/ex4.js
@@ -0,0 +1,9 @@
+/* Write a function that accepts a single argument, a number,
+ and returns the result of multiplying its argument
+ by an exponent of 2 (also known as squaring the number). */
+
+ let square = function(number) {
+    return number ** 2;
+ }
+
+ console.log(square(3));
