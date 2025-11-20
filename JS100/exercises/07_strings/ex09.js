5aa328253ba5b1c163683c094659eb6b73dfd7d1
Tim <N/A>
2025-11-20 19:18:48 +0000

    changed logic to capitalize array elements and then join them into a string

--- JS100/exercises/07_strings/ex09.js
+++ JS100/exercises/07_strings/ex09.js
@@ -5,11 +5,10 @@ const strInput = 'launch school tech & talk';
 
 function capInitials(str) {
     let words = str.split(" ");
-    let output = "";
     for (let i = 0; i < words.length; i += 1) {
-        output = output + " " + words[i].charAt(0).toUpperCase() + words[i].slice(1, words[i].length);
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1, words[i].length);
     }
-    console.log(output);
+    return words.join(" ");
 }
 
-capInitials(strInput);
+console.log(capInitials(strInput));
