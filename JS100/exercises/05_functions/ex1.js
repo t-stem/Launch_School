2bf97cb21a08e54a4d4f26d9c9f29e58422b15f8
Tim <N/A>
2025-11-20 13:28:35 +0000

    submitting functions ex1

---
+++ JS100/exercises/05_functions/ex1.js
@@ -0,0 +1,10 @@
+/* Examine the example function invocation below. Write the function sum, 
+such that it accepts two arguments and returns the sum of its arguments. 
+You may assume that the function arguments will always be numbers. */
+
+function sum(x, y) {
+    return x + y;
+}
+
+let z = sum(22, 10); // 32
+console.log(z);
