02f64f711fb36a88f54a2fd815d45f9edd1771f7
Tim <N/A>
2025-11-20 18:10:28 +0000

    submitting strings ex06

---
+++ JS100/exercises/07_strings/ex06.js
@@ -0,0 +1,5 @@
+/* Write code that checks whether the string byteSequence contains the character x */
+
+let byteSequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu';
+
+byteSequence.indexOf("x") > -1 ? console.log("byteSequence contains 'x'.") : console.log("byteSequence doesn't contain 'x'.");
