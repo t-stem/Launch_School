4f508a110e5138af0fcd55c15a14c205d6534b52
Tim <N/A>
2025-11-20 17:32:15 +0000

    submitting strings ex04

---
+++ JS100/exercises/07_strings/ex04.js
@@ -0,0 +1,5 @@
+/* How can you assign this string to a single variable, preserving the line break? */
+
+const string = "A pirate I was meant to be!\nTrim the sails and roam the sea!"
+
+console.log(string);
