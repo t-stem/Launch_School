d6467078821121a09f904722682bb983d2d32c42
Tim <N/A>
2025-11-21 16:45:55 +0000

    submitting functions2 ex04

---
+++ JS100/exercises/09_functions2/ex04.js
@@ -0,0 +1,19 @@
+/* Create a function that calculates a person's body mass index (BMI). 
+It should take two parameters: someone's height (in cm) and weight (in kg). 
+The formula for calculating the BMI is as follows:
+
+bmi = weightInKilograms / heightInMeters**2;
+
+Note that formula requires a height in meters, but the function takes the height in centimeters (1 meter is equivalent to 100 centimeters).
+
+Return the result as a string rounded to two decimals. For example:
+
+*/
+
+function calculateBMI(heightInCentimeters, weightInKilograms) {
+    heightInMeters = heightInCentimeters / 100;
+    return weightInKilograms / heightInMeters ** 2;
+}
+
+console.log(calculateBMI(180, 80)); // "24.69"
+
