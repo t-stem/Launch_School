95ddd610b7e403ca6ce2dc59f50a4a586598790b
Tim <N/A>
2025-12-01 16:37:38 +0000

    replaced negative integer check with Math.abs() simplified return statement logic added test case for nr = 0

--- JS101/small_problems/easy1/ex01.js
+++ JS101/small_problems/easy1/ex01.js
@@ -3,13 +3,15 @@ This method returns true if the number's absolute value is odd.
 You may assume that the argument is a valid integer value. */
 
 const checkAbsOdd = function(nr) {
-    if (nr < 0) {
-        nr = nr * -1;
-    }
-    return !(nr % 2 === 0);
+    // if (nr < 0) {
+    //    nr = nr * -1;
+    //}
+    nr = Math.abs(nr); // improvement: use Math object
+    return (nr % 2 !== 0); // simplification
 }
 
 // test
 console.log(checkAbsOdd(-3));
 console.log(checkAbsOdd(5));
 console.log(checkAbsOdd(4));
+console.log(checkAbsOdd(0)); // improvement: added test case for nr = 0
