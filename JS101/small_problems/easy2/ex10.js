080c5e9dba6e5236de849e9674e5db5d1f56a07b
Tim <N/A>
2025-12-02 16:16:24 +0000

    submitting easy2 ex10

---
+++ JS101/small_problems/easy2/ex10.js
@@ -0,0 +1,23 @@
+/* Build a program that logs when the user will retire and how many more years
+ the user has to work until retirement. 
+*/
+
+let readlineSync = require("readline-sync");
+
+const CURRENT_YEAR = 2017;
+let currentAge = readlineSync.question("What is your age? ");
+let retirementAge = readlineSync.question("At what age would you like to retire? ");
+let yearsToRetirement = retirementAge - currentAge;
+let retirementYear = CURRENT_YEAR + yearsToRetirement;
+
+console.log(`It's ${CURRENT_YEAR}. You will retire in ${retirementYear}.
+You have only ${yearsToRetirement} years of work to go!`);
+
+
+/*
+What is your age? 30
+At what age would you like to retire? 70
+
+It's 2017. You will retire in 2057.
+You have only 40 years of work to go!
+*/
