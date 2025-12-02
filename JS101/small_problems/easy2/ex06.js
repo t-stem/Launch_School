e11e5b15460670fd5fcc4f445ed539a6c62e0e3f
Tim <N/A>
2025-12-02 15:10:48 +0000

    correction: changed split parameter to regex /\s+/ where \s reference to a blank character and + refers to 1 or more occurances of this character

--- JS101/small_problems/easy2/ex06.js
+++ JS101/small_problems/easy2/ex06.js
@@ -7,9 +7,10 @@ You may assume that the input String will always contain at least two words.
 */
 
 function penultimate(stringArg) {
-    let stringArray = stringArg.split(" ");
+    let stringArray = stringArg.split(/\s+/); // correction: changed split parameter to regex /\s+/ where \s reference to a blank character and + refers to 1 or more occurances of this character 
     return stringArray[stringArray.length - 2];
 }
 
 console.log(penultimate("last word") === "last"); // logs true
 console.log(penultimate("Launch School is great!") === "is"); // logs true
+console.log(penultimate("i am  going    home"));
