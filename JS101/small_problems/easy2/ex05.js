17f5fa7c1260abc93f45005362c2d4a9fec6de9a
Tim <N/A>
2025-12-02 14:26:14 +0000

    submitting easy2 ex05

---
+++ JS101/small_problems/easy2/ex05.js
@@ -0,0 +1,30 @@
+/* Write a program that prompts the user for two positive integers, 
+and then prints the results of the following operations on those two numbers: 
+addition, subtraction, product, quotient, remainder, and power. 
+
+Do not worry about validating the input.*/ 
+
+let readlineSync = require("readline-sync");
+
+let int1 = readlineSync.question("==> Enter the first number: ");
+let int2 = readlineSync.question("==> Enter the second number: ");
+
+console.log(`==> ${int1} + ${int2} = ${int1 + int2}`);
+console.log(`==> ${int1} - ${int2} = ${int1 - int2}`);
+console.log(`==> ${int1} * ${int2} = ${int1 * int2}`);
+console.log(`==> ${int1} / ${int2} = ${int1 / int2}`);
+console.log(`==> ${int1} % ${int2} = ${int1 % int2}`);
+console.log(`==> ${int1} ** ${int2} = ${int1 ** int2}`);
+
+/*
+==> Enter the first number:
+23
+==> Enter the second number:
+17
+==> 23 + 17 = 40
+==> 23 - 17 = 6
+==> 23 * 17 = 391
+==> 23 / 17 = 1
+==> 23 % 17 = 6
+==> 23 ** 17 = 1.4105003956066297e+23
+*/
