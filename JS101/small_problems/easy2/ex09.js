123c57dadaf01eab2538c19aed9bf529174b1662
Tim <N/A>
2025-12-02 16:05:04 +0000

    submitting easy2 ex09

---
+++ JS101/small_problems/easy2/ex09.js
@@ -0,0 +1,10 @@
+/* Build a program that randomly generates Teddy's age, and logs it to the console. 
+Have the age be a random number between 20 and 120 (inclusive).
+*/
+let randomNumber = Math.floor(Math.random() * (120 - 20) + 20);
+
+console.log(`Teddy is ${randomNumber} years old!`);
+
+
+
+// Teddy is 69 years old!
