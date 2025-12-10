b07c5228e45586e3d39a4c769c971b6448dc1830
Tim <N/A>
2025-12-10 14:51:46 +0000

    submitting js101 easy3 ex02

---
+++ JS101/small_problems/easy3/ex02.js
@@ -0,0 +1,34 @@
+/* Write a function that will take a short line of text, and write it to the console log within a box. */
+
+let template = '';
+
+
+
+function logInBox(text) {
+    const TEXT_LENGTH = text.length;
+    let lineOuter = `+-${"-".repeat(TEXT_LENGTH)}-+`;
+    let lineInner = `| ${" ".repeat(TEXT_LENGTH)} |`;
+    let lineCenter = `| ${text} |`;
+    console.log(lineOuter + '\n' + lineInner + '\n' + lineCenter + '\n' + lineInner + '\n' + lineOuter);
+}
+
+logInBox('To boldly go where no one has gone before.');
+
+/*
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+*/
+
+logInBox('');
+
+/*
+logInBox('');
++--+
+|  |
+|  |
+|  |
++--+
+*/
