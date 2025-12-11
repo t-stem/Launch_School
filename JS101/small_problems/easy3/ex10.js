6cb7e1646d006c5275db62b368d2e6b12beaaeb7
Tim <N/A>
2025-12-11 10:36:10 +0000

    submitting js101 easy3 ex10

---
+++ JS101/small_problems/easy3/ex10.js
@@ -0,0 +1,39 @@
+/* 
+
+Write a function that takes a year as input and returns the century. 
+The return value should be a string that begins with the century number, 
+and ends with 'st', 'nd', 'rd', or 'th' as appropriate for that number.
+
+New centuries begin in years that end with 01. So, the years 1901 - 2000 comprise the 20th century. 
+*/
+
+function centuryEnding(century) {
+    const exceptionsLookup = Object.freeze({1: 'st', 2: 'nd', 3: 'rd', 21: 'st', 22: 'nd', 23: 'rd'});
+    const endingExceptions = Object.keys(exceptionsLookup);
+    const centuryString = String(century);
+    return endingExceptions.includes(centuryString) ? exceptionsLookup[centuryString] : 'th';
+    
+}
+
+function century (year) {  
+    let moduloCentury = year % 100;
+    if (moduloCentury === 0) {
+        return year / 100 + centuryEnding(year / 100);
+    } else {
+        let truncCentury = Math.trunc(year / 100) + 1;
+        return String(truncCentury) + centuryEnding(truncCentury);
+    }
+
+
+}
+
+console.log(century(2000));        // "20th"
+console.log(century(2001));        // "21st"
+console.log(century(2001));        // "21st"
+console.log(century(1965));        // "20th"
+console.log(century(256));         // "3rd"
+console.log(century(5));           // "1st"
+console.log(century(10103));       // "102nd"
+console.log(century(1052));        // "11th"
+console.log(century(1127));        // "12th"
+console.log(century(11201));       // "113th"
