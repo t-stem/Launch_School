c26bac29a61e0c6cbb38443f731ea4b8a65f61e9
Tim <N/A>
2026-01-05 18:42:40 +0000

    submitting easy4 ex07

---
+++ js110/small_problems/easy4/ex07.js
@@ -0,0 +1,36 @@
+/* Modify the wordSizes function from the previous exercise to exclude non-letters when determining word size.
+
+For instance, the word size of "it's" is 3, not 4.
+*/
+
+function cleanWordLength(word) {
+  return word.replace(/[^a-z]/gi, "").length;
+}
+
+
+function wordSizes(inputString) {
+  let lengthCounts = {};
+
+  inputString
+  .split(" ")
+  .forEach(word => {
+    let len = cleanWordLength(word);
+    let lenNotZero = len !== 0;
+
+    if (!lengthCounts[len] && lenNotZero) {
+      lengthCounts[len] = 1;
+    } else if (lenNotZero) {
+      lengthCounts[len] += 1;
+    }
+  });
+  
+  return lengthCounts;
+}
+
+
+
+// Examples:
+console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 2 }
+console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 3 }
+console.log(wordSizes("What's up doc?"));                              // { "2": 1, "3": 1, "5": 1 }
+console.log(wordSizes(''));                                            // {}
