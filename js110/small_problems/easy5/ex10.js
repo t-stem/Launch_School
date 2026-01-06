1a9eee0b928dd0b2ccb67cd8c138fbdafd6f9c7c
Tim <N/A>
2026-01-06 18:06:00 +0000

    submitting easy5 ex10

---
+++ js110/small_problems/easy5/ex10.js
@@ -0,0 +1,15 @@
+/*
+Write a function that takes one argument, an array of integers, and returns the average of all the integers in the array, 
+rounded down to the integer component of the average. The array will never be empty, 
+and the numbers will always be positive integers.
+*/
+
+function average(integersArray) {
+  let countIntegers = integersArray.length;
+  let arraySum = integersArray.reduce((accumulator, integer) => accumulator += integer, 0);
+
+  return Math.floor(arraySum / countIntegers);
+}
+
+console.log(average([1, 5, 87, 45, 8, 8]));       // 25
+console.log(average([9, 47, 23, 95, 16, 52]));    // 40
