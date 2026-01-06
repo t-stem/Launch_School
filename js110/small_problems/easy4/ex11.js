d1f0bbed5dbd8977828af3141ca9a0a7f821e4f5
Tim <N/A>
2026-01-06 11:49:31 +0000

    REVISIT FEEDBACK LATER

--- js110/small_problems/easy4/ex11.js
+++ js110/small_problems/easy4/ex11.js
@@ -7,7 +7,7 @@ the string representation of that integer.
 
 You may not use any of the standard conversion functions available in JavaScript, such as String(), 
 Number.prototype.toString, or an expression such as '' + number. Your function should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
-*/
+revisit later*/
 
 function integerToString (inputInteger) {
   const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; // uses property of arrays that index numbers are of number type
