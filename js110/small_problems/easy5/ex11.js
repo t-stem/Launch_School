9558bcfff214071f7ac7f3995865a04fa3846879
Tim <N/A>
2026-01-06 19:04:57 +0000

    CORRECT BUT REVISIT FEEDBACK AND REVISE LATER

--- js110/small_problems/easy5/ex11.js
+++ js110/small_problems/easy5/ex11.js
@@ -6,6 +6,8 @@ If the number of minutes is negative, the time is before midnight.
 Write a function that takes a time using this minute-based format and returns the time of day in 24 hour format (hh:mm). Your function should work with any integer input.
 
 You may not use javascript's Date class methods.
+
+revisit
 */
 
 function toTwoDigits (integer) {
