736bedfac7bb8d89fa22aea4cd18942f2d408b09
Tim <N/A>
2026-01-06 15:53:54 +0000

    submitting easy5 ex08.js

---
+++ js110/small_problems/easy5/ex08.js
@@ -0,0 +1,26 @@
+/* 
+Write a function that takes one argument, a positive integer, and returns a list of the digits in the number.
+*/
+
+function digitList(inputNumber) {
+  const BASE = 10;
+  let outputArray = []
+  let currentNumber = inputNumber;
+  
+  do {
+    let currentDigit = currentNumber % BASE;
+    
+    outputArray.unshift(currentDigit);
+    
+    currentNumber = (currentNumber - currentDigit) / BASE;
+  }
+  while (currentNumber > 0)
+
+  return outputArray;
+}
+
+
+console.log(digitList(12345));       // [1, 2, 3, 4, 5]
+console.log(digitList(7));           // [7]
+console.log(digitList(375290));      // [3, 7, 5, 2, 9, 0]
+console.log(digitList(444));         // [4, 4, 4]
