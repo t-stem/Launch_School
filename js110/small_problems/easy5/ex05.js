000b2f405edcf6af17a8791712e059fb2250a02b
Tim <N/A>
2026-01-06 15:14:03 +0000

    added alternative solution

--- js110/small_problems/easy5/ex05.js
+++ js110/small_problems/easy5/ex05.js
@@ -5,7 +5,7 @@ and returns a new array that contains all elements from both array arguments, wi
 You may assume that both input arrays are non-empty, and that they have the same number of elements.
 */
 
-function interleave(array1, array2) {
+function interleave2(array1, array2) {
   let newArray = [];
 
   for (let i = 0; i < array1.length; i +=1) {
@@ -16,6 +16,19 @@ function interleave(array1, array2) {
     newArray[j * 2 + 1] = array2[j];
   }
 
+  return newArray;
+}
+
+function interleave(array1, array2) { // ALTERNATIVE: since we assume the arrays are equal length, we can consider the elements in the two arrays as pairs
+  let newArray = [];
+
+  for (let element of array1) {
+    newArray.push(element);
+
+    let index = array1.indexOf(element);
+    newArray.push(array2[index]);
+  }
+  
   return newArray;
 }
 
