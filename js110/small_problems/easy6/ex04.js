96f0e3531e44820594ac9c1e4b46db0a63c83147
Tim <N/A>
2026-01-09 10:54:58 +0000

    submitting easy6 ex04.js

---
+++ js110/small_problems/easy6/ex04.js
@@ -0,0 +1,19 @@
+/* 
+Write a function that takes an integer argument and returns an array containing all integers between 1 and the argument (inclusive), in ascending order.
+
+You may assume that the argument will always be a positive integer.
+*/
+
+function sequence(inputInteger) {
+  let integerArray = [];
+
+  for (i = 1; i <= inputInteger; i += 1) {
+    integerArray.push(i);
+  }
+
+  return integerArray;
+}
+
+console.log(sequence(5));    // [1, 2, 3, 4, 5]
+console.log(sequence(3));    // [1, 2, 3]
+console.log(sequence(1));    // [1]
