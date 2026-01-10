1188610b4a496e18f5a691a78c73a30938e6f651
Tim <N/A>
2026-01-10 17:33:24 +0000

    submitting easy6 ex10

--- js110/small_problems/easy6/ex10.js
+++ js110/small_problems/easy6/ex10.js
@@ -3,29 +3,89 @@ Write a function that takes a string as an argument and returns true if all pare
 false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
 */
 
-function pairedParentheses(inputString) {
-  // console.log('Pairing')
-  for (i = 0; i < inputString.length; i += 2) {
-    let subString = inputString[i] + inputString[i+1];
+function pairedParentheses2(inputString) { // DEPRECATED: this function doesn't return the required result
+  let openCounter = 0;
+  let closeCounter = 0;
+  let index = 0;
 
-    if (subString !== '()') {
+  while (index < inputString.length) {
+    openCounter = 0;
+    closeCounter = 0;
+
+    while (inputString[index] === "(") {
+      openCounter += 1;
+      index += 1;
+    }
+  
+    while (inputString[index] === ')') {
+      closeCounter += 1;
+      index += 1;
+    }
+
+    if (openCounter !== closeCounter) {
       return false;
     }
   }
+  
+  return leftCounter === rightCounter;
+}
+
+function pairedParentheses3(inputString) { // DEPRECATED: this function doesn't return the desired result
+  const OPEN = '(';
+  const CLOSE = ')';
+  let newString = inputString;
+
+  while (newString.length > 0) {
+    let firstOpen = newString.indexOf(OPEN);
+    let lastOpen = newString.lastIndexOf(OPEN);
+    let firstClose = newString.indexOf(CLOSE);
+    let lastClose = newString.lastIndexOf(CLOSE);
+
+    if ((firstOpen === -1 || firstClose === -1) && (firstOpen !== firstClose)) {
+      return false;
+    } else if (firstOpen > firstClose || lastOpen > lastClose) {
+      return false;
+    }
+
+    newString = inputString.slice(firstOpen + 1, lastClose);
+  }
+  
   return true;
 }
 
+function pairedParentheses(inputString) {
+  let balancedCounter = 0;
+  
+  for (let char of inputString) {
+    if (char === '(') {
+      balancedCounter += 1;
+
+    } else if (char === ')') {
+      balancedCounter -= 1;
+    }
+
+    if (balancedCounter < 0) {
+      return false;
+    }
+  }
+  return balancedCounter === 0;
+
+}
+
 /* Test cases pairedParentheses
-console.log(pairedParentheses('()'));
-console.log(pairedParentheses('()()'));
-console.log(pairedParentheses(')('));
-console.log(pairedParentheses('()(('));
+console.log(pairedParentheses('()')); // true
+console.log(pairedParentheses('()()')); // true
+console.log(pairedParentheses('(())')); // true
+console.log(pairedParentheses('()(())')); // true
+console.log(pairedParentheses('()(()')); // false
+console.log(pairedParentheses(')(')); // false
+console.log(pairedParentheses('()((')); // false
+console.log(pairedParentheses('(()())')); // true
 */
 
 function isBalanced(inputString) {
   let sanitizedString = inputString.replace(/[^()]/g, "");
   let sanitizedStringLength = sanitizedString.length;
-  console.log(sanitizedStringLength)
 
   if (sanitizedStringLength === 0) {
     return true;
