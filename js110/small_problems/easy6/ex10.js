8e281c9a966a75660d41b1d11a52199fc5782dda
Tim <N/A>
2026-01-10 17:35:04 +0000

    REVISE CORRECT SOLUTION

--- js110/small_problems/easy6/ex10.js
+++ js110/small_problems/easy6/ex10.js
@@ -1,6 +1,7 @@
-/*
+/* revise 
 Write a function that takes a string as an argument and returns true if all parentheses in the string are properly balanced, 
 false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
+
 */
 
 function pairedParentheses2(inputString) { // DEPRECATED: this function doesn't return the required result
