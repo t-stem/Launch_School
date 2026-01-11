2b818855d2ae8a33307aec697d968021471c80c3
Tim <N/A>
2026-01-11 16:51:16 +0000

    submitting strings and text processing ex04

--- js110/small_problems/string_text_processing/ex04.js
+++ js110/small_problems/string_text_processing/ex04.js
@@ -6,7 +6,7 @@ You may assume that a word is any sequence of non-whitespace characters.
 */
 
 function wordCap(inputString) {
-  return wordsArray = inputString
+  return inputString
   .split(" ")
   .map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase())
   .join(" ");
