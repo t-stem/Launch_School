094729b7253e4eccd1c35d978286883bc8aea064
Tim <N/A>
2026-01-11 16:49:20 +0000

    submitting string and text processing ex04

---
+++ js110/small_problems/string_text_processing/ex04.js
@@ -0,0 +1,18 @@
+/*
+Write a function that takes a string as an argument and returns that string with the first character of every word 
+capitalized and all subsequent characters in lowercase.
+
+You may assume that a word is any sequence of non-whitespace characters.
+*/
+
+function wordCap(inputString) {
+  return wordsArray = inputString
+  .split(" ")
+  .map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase())
+  .join(" ");
+}
+
+console.log(wordCap('four score and seven'));       // "Four Score And Seven"
+console.log(wordCap('the javaScript language'));    // "The Javascript Language"
+console.log(wordCap('this is a "quoted" word'));    // 'This Is A "quoted" Word'
+
