7336b039c155d032cfca72f4be8fcc6cd4f5bd5a
Tim <N/A>
2026-01-11 17:12:13 +0000

    alternative with .replace(/\S+/) where S is any sequence of non-whitespace chars

--- js110/small_problems/string_text_processing/ex04.js
+++ js110/small_problems/string_text_processing/ex04.js
@@ -5,11 +5,15 @@ capitalized and all subsequent characters in lowercase.
 You may assume that a word is any sequence of non-whitespace characters.
 */
 
-function wordCap(inputString) {
+function wordCap1(inputString) { // this solution colapses multiple white spaces between words into one space
   return inputString
   .split(" ")
   .map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase())
   .join(" ");
+}
+
+function wordCap(inputString) { // this version preserves multiple white spaces
+  return inputString.replace(/\S+/g, word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()) // /\S+/ matches any sequece of non-whitespace chars, where S stands for a non-whitespace char (s stands for a whitespace char)
 }
 
 console.log(wordCap('four score and seven'));       // "Four Score And Seven"
