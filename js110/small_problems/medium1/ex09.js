4e1eea5a1b0e4baee50182eddde6d50eb6b01b12
Tim <N/A>
2026-01-15 13:43:00 +0000

    submitting js110 medium1 ex09

---
+++ js110/small_problems/medium1/ex09.js
@@ -0,0 +1,49 @@
+/*
+Our recursive fibonacci function from an earlier exercise isn't very efficient. 
+
+It starts slowing down with an nth argument value as low as 35. 
+
+One way to improve the performance of our recursive fibonacci function (and other recursive functions) is to use memoization.
+
+Memoization is an approach that involves saving a computed answer for future reuse, instead of computing it from scratch every time it is needed. 
+In the case of our recursive fibonacci function, using memoization saves calls to fibonacci(nth - 2) because the necessary values have already been computed by the recursive calls to fibonacci(nth - 1).
+
+For this exercise, your objective is to refactor the recursive fibonacci function to use memoization.
+*/
+
+function fibonacciSequence(n) {
+  let sequence = [null, 1, 1];
+  
+  function checkSequence(N) {
+    if (sequence[N]){
+      return sequence[N];
+
+    } else {
+      let fib = fibonacci(N);
+      sequence.push(fib);
+      
+      return fib;
+    }
+  }
+
+  function fibonacci(n) {
+    if (n > 2) {
+      let fib1 = checkSequence(n - 1);
+      let fib2 = checkSequence(n - 2);
+      
+      return fib1 + fib2; 
+    }
+    
+    return 1;
+  }
+
+  return fibonacci(n);
+}
+
+console.log(fibonacciSequence(1));       // 1
+console.log(fibonacciSequence(2));       // 1
+console.log(fibonacciSequence(3));       // 2
+console.log(fibonacciSequence(4));       // 3
+console.log(fibonacciSequence(5));       // 5
+console.log(fibonacciSequence(12));      // 144
+console.log(fibonacciSequence(75));      // 2111485077978050
