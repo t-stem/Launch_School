f8ab65fdedaebb92c78cf56cfde0ce427d259bd1
Tim <N/A>
2026-01-15 19:05:38 +0000

    submitting js110 medium1 ex02

--- js110/small_problems/medium2/ex02.js
+++ js110/small_problems/medium2/ex02.js
@@ -6,22 +6,24 @@ Isosceles: Two sides are of equal length, while the third is different.
 Scalene: All three sides are of different lengths.
 
 To be a valid triangle, the sum of the lengths of the two shortest sides must be greater than the length of the longest side,
+and every side must have a length greater than 0. 
+If either of these conditions is not satisfied, the triangle is invalid.
  
 Write a function that takes the lengths of the three sides of a triangle as arguments and returns one of the following four strings representing the triangle's classification: 
 'equilateral', 'isosceles', 'scalene', or 'invalid'.
 */
 
 function triangle(side1, side2, side3) {
-  if (side1 === 0 || side2 === 0 || side3 === 0) return 'invalid';
+  if (side1 <= 0 || side2 <= 0 || side3 <= 0) return 'invalid'; // replaced === by <= to match problem description
 
   let sorted = [side1, side2, side3].sort((a, b) => a - b);
-  if (sorted[0] + sorted[1] < sorted[2]) return 'invalid';
+  if (sorted[0] + sorted[1] <= sorted[2]) return 'invalid'; // replaced < by <= to match problem description
   
   if (side1 === side2 && side2 === side3) return 'equilateral';
 
   if (side1 === side2 || side1 === side3 || side2 === side3) return 'isosceles';
 
-  return 'scalence';
+  return 'scalene';
 }
 
 console.log(triangle(3, 3, 3));        // "equilateral"
