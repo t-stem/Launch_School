a9394d27d1d5809073a3799d9741fe2421d76d92
Tim <N/A>
2026-01-15 14:06:28 +0000

    submitting js110 medium1 ex08

--- js110/small_problems/medium1/ex08.js
+++ js110/small_problems/medium1/ex08.js
@@ -1,44 +1,33 @@
 /*
-The Fibonacci series is a sequence of numbers in which each number is the sum of the previous two numbers. 
-The first two Fibonacci numbers are 1 and 1. The third number is 1 + 1 = 2, the fourth is 1 + 2 = 3, the fifth is 2 + 3 = 5, and so on. 
+In the previous exercise, we developed a recursive solution for computing the nth Fibonacci number. 
 
-In mathematical terms, this can be represented as:
+In a language that is not optimized for recursion, some (but not all) recursive functions can be extremely 
+slow and may require massive quantities of memory and/or stack space. If you tested for bigger nth numbers, 
+you might have noticed that getting the 50th fibonacci number already takes a significant amount of time.
 
-F(1) = 1
-F(2) = 1
-F(n) = F(n - 1) + F(n - 2) // where n > 2
+Fortunately, every recursive function can be rewritten as a non-recursive (or procedural) function.
 
-This simple sequence can be computed using a recursive function. A recursive function is one in which the function calls itself. 
-For example, the following function is a recursive function that computes the sum of all integers between 1 and n:
+Rewrite your recursive fibonacci function so that it computes its results without using recursion.
 
-
-function sum(num) {
-  if ( num === 1) {
-    return 1;
-  }
-  return num + sum(num - 1);
-}
-
-A good recursive function has three primary qualities:
-
-It calls itself at least once.
-It has an ending condition — e.g., if (num === 1), in the sum function above.
-The results of each recursion are used in each step — e.g., num + sum(num - 1) uses sum(num - 1).
-Write a recursive function that computes the nth Fibonacci number, where nth is an argument passed to the function.
 */
 
-function fibonacci(inputNumber) {
-  if (inputNumber > 2) return fibonacci(inputNumber - 1) + fibonacci(inputNumber - 2);
+function fibonacci(n) {
+  if (n <= 2) return 1;
 
-  return 1;
+  let min1 = 1;
+  let min2 = 1;
+  let fib;
+
+  for (let i = 3; i <= n; i += 1) {
+    fib = min1 + min2;
+    min2 = min1;
+    min1 = fib;
+  }
+
+  return fib;
 }
 
 
-
-console.log(fibonacci(1));       // 1
-console.log(fibonacci(2));       // 1
-console.log(fibonacci(3));       // 2
-console.log(fibonacci(4));       // 3
-console.log(fibonacci(5));       // 5
-console.log(fibonacci(12));      // 144
-console.log(fibonacci(20));      // 6765
+console.log(fibonacci(20));       // 6765
+console.log(fibonacci(50));       // 12586269025
+console.log(fibonacci(75));       // 2111485077978050
