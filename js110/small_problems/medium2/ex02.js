f36400485ccb626d54c10fbdc4d894b1922ec80e
Tim <N/A>
2026-01-15 19:41:30 +0000

    submitting js110 medium2 ex03

--- js110/small_problems/medium2/ex02.js
+++ js110/small_problems/medium2/ex02.js
@@ -26,6 +26,19 @@ function triangle(side1, side2, side3) {
   return 'scalene';
 }
 
+function triangle2(side1, side2, side3) {
+  if (side1 <= 0 || side2 <= 0 || side3 <= 0) return 'invalid'; // replaced === by <= to match problem description
+
+  let [shortest, middle, largest] = [side1, side2, side3].sort((a, b) => a - b); // improvement: repharsed with new syntax to make code more intuitive
+  if (shortest + middle <= largest) return 'invalid'; // replaced < by <= to match problem description
+  
+  if (side1 === side2 && side2 === side3) return 'equilateral';
+
+  if (side1 === side2 || side1 === side3 || side2 === side3) return 'isosceles';
+
+  return 'scalene';
+}
+
 console.log(triangle(3, 3, 3));        // "equilateral"
 console.log(triangle(3, 3, 1.5));      // "isosceles"
 console.log(triangle(3, 4, 5));        // "scalene"
---
+++ js110/small_problems/medium2/ex03.js
@@ -0,0 +1,33 @@
+/*
+A triangle is classified as follows:
+
+Right: One angle is a right angle (exactly 90 degrees).
+Acute: All three angles are less than 90 degrees.
+Obtuse: One angle is greater than 90 degrees.
+To be a valid triangle, the sum of the angles must be exactly 180 degrees, and every angle must be greater than 0. If either of these conditions is not satisfied, the triangle is invalid.
+
+Write a function that takes the three angles of a triangle as arguments and returns one of the following four strings representing the triangle's classification: 'right', 'acute', 'obtuse', or 'invalid'.
+
+You may assume that all angles have integer values, so you do not have to worry about floating point errors. You may also assume that the arguments are in degrees.
+
+*/
+
+function triangle(angle1, angle2, angle3) {
+  [smallest, middle, largest] = [angle1, angle2, angle3].sort((a, b) => a - b);
+
+  if (smallest <= 0) return 'invalid';
+
+  if (smallest + middle + largest !== 180) return 'invalid';
+
+  if (largest > 90) return 'obtuse';
+
+  if (largest === 90) return 'right';
+
+  return 'acute';
+}
+
+console.log(triangle(60, 70, 50));       // "acute"
+console.log(triangle(30, 90, 60));       // "right"
+console.log(triangle(120, 50, 10));      // "obtuse"
+console.log(triangle(0, 90, 90));        // "invalid"
+console.log(triangle(50, 50, 50));       // "invalid"
