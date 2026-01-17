e4affcccde661b25aedb343c1d51120e6872f84b
Tim <N/A>
2026-01-17 09:56:10 +0000

    submitting js110 debugging js ex01

---
+++ js110/small_problems/debugging_js/ex01.js
@@ -0,0 +1,32 @@
+/*
+Gemma and some friends are working on a complex program to generate word ladders, 
+transforming one word into another word one character at a time. 
+
+The smallest of her tasks is to print the resulting ladder to the screen.
+
+A "ladder" is simply an array of word strings; 
+Gemma decides to transform this array into a single string where each word within the string
+is separated by a hyphen ('-'). 
+
+For example, the array ['pig', 'pie', 'lie', 'lit', 'let'] should be printed as the string 'pig-pie-lie-lit-let'.
+
+Upon first glance, Gemma's code below looks like it should work. But it throws a TypeError, saying: Cannot read property 'forEach' of undefined. Why is that?
+*/
+
+
+/* ANSWER:
+it seems like the .forEach method somehow does't recognize the array and is trying to access properties of 'undefined. The second line of the stack trace says Object.<anonymous>, 
+which seems like the Object.prototype where prototype is not provided. 
+
+
+let ladder = ''
+
+['head', 'heal', 'teal', 'tell', 'tall', 'tail'].forEach(word => {
+  if (ladder !== '') {
+    ladder += '-'
+  }
+
+  ladder += word
+})
+
+console.log(ladder)  // expect: head-heal-teal-tell-tall-tail
