c1fbc7080e60bcfbbc7536f2edf75a798409eaf2
Tim <N/A>
2026-01-22 12:18:34 +0000

    submitting js110 debugging_js ex08

---
+++ js110/small_problems/debugging_js/ex08.js
@@ -0,0 +1,45 @@
+/* ANSWER: 
+- the mistake is in the num.sort() statement
+- the sort method doesn't have a callback function
+- in the absence of a callback, the method will sort the array after converting all elements to strings
+- it will take the UTF-16 values of the strings and use those to sort the array
+- this will lead to a different outcome than sorting the array based on the actual numerical values
+*/
+
+function average(nums) {
+  let sum = nums.reduce((total, num) => total + num);
+
+  return sum / nums.length;
+}
+
+function median(nums) {
+  nums.sort((a, b) => a - b);
+
+  let median;
+  let length = nums.length;
+  if (length % 2 === 0) {
+    median = average([nums[(length / 2) - 1], nums[length / 2]]);
+  } else {
+    median = nums[Math.floor(length / 2)];
+  }
+
+  return median;
+}
+
+// Tests
+
+let quarter1ExamScores = [89, 72, 100, 93, 64, 97, 82, 87, 90, 94];
+let quarter2ExamScores = [76, 91, 89, 90, 91, 67, 99, 82, 91, 87];
+let quarter3ExamScores = [99, 91, 88, 72, 76, 64, 94, 79, 86, 88];
+let quarter4ExamScores = [100, 94, 73, 88, 82, 91, 97, 99, 80, 84];
+
+// should all log 'true':
+console.log(average(quarter1ExamScores) === 86.8);
+console.log(average(quarter2ExamScores) === 86.3);
+console.log(average(quarter3ExamScores) === 83.7);
+console.log(average(quarter4ExamScores) === 88.8);
+
+console.log(median(quarter1ExamScores) === 89.5);
+console.log(median(quarter2ExamScores) === 89.5);
+console.log(median(quarter3ExamScores) === 87);
+console.log(median(quarter4ExamScores) === 89.5);
