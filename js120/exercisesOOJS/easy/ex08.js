7edd61a57cd09f6f41cb2aa6d4900b8b40c4a5bf
Tim <N/A>
2026-02-03 22:27:31 +0000

    submitting js120 easy ex08

---
+++ js120/exercisesOOJS/easy/ex08.js
@@ -0,0 +1,51 @@
+/*
+You have the following classes.
+*/
+
+class Creature {
+  constructor(name) {
+    this.name = name;
+  }
+
+  walk() {
+    return `${this.name} ${this.gait()} forward`;
+  }
+}
+
+class Person extends Creature {
+
+  gait() {
+    return "strolls";
+  }
+}
+
+class Cat extends Creature {
+
+  gait() {
+    return "saunters";
+  }
+}
+
+class Cheetah extends Creature {
+
+  gait() {
+    return "runs";
+  }
+}
+
+/*
+You need to modify the code so that this works:
+You are only allowed to write one new method to do this
+*/
+
+let mike = new Person("Mike");
+console.log(mike.walk());
+// "Mike strolls forward"
+
+let kitty = new Cat("Kitty");
+console.log(kitty.walk());
+// "Kitty saunters forward"
+
+let flash = new Cheetah("Flash");
+console.log(flash.walk());
+// "Flash runs forward"
