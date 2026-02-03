62e1d0ef267e46c95e5ccf174349c04f5a71df9e
Tim <N/A>
2026-02-03 14:12:25 +0000

    submitting js120 basics classes ex05

---
+++ js120/exercisesOOJS/basics_classes/ex05.js
@@ -0,0 +1,17 @@
+/*
+Using the code from the previous exercise, add a parameter to constructor that provides a name for the Cat object. 
+
+Assign this parameter to a property called name and use it to log a greeting with the provided name. 
+
+(You can remove the code that displays I'm a cat!.)
+*/
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+    console.log(`My name is ${this.name}!`);
+  }
+}
+
+let kitty = new Cat('Sophie');
+
