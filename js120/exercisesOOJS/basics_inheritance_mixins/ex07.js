010575a9da2b1bca6e407de194570a487e3cded5
Tim <N/A>
2026-02-03 17:15:06 +0000

    submitting js120 basics inheritance mixins ex07

---
+++ js120/exercisesOOJS/basics_inheritance_mixins/ex07.js
@@ -0,0 +1,19 @@
+/*
+Using the following code, create a towMixin mixin that contains a method named tow that returns I can tow a trailer! 
+when invoked. Include the mixin in the Truck class.
+*/
+
+const towMixin = {
+  tow() {
+    return 'I can tow a trailer!';
+  }
+}
+
+
+class Truck {}
+Object.assign(Truck.prototype, towMixin);
+
+class Car {}
+
+let truck = new Truck();
+console.log(truck.tow());
