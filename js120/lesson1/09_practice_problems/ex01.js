e8b59b58d6187d4ede83e33c6b588364eb3eeb1e
Tim <N/A>
2026-02-07 18:47:44 +0000

    submitting js120 lesson1 assignment9 ex01

---
+++ js120/lesson1/09_practice_problems/ex01.js
@@ -0,0 +1,47 @@
+/*
+Attributes
+  Title: Mythos
+  Author: Stephen Fry
+
+Behavior:
+  Get Description
+
+-----------------------------
+Attributes
+  Title: Me Talk Pretty One Day
+  Author: David Sedaris
+
+Behavior:
+  Get Description
+
+-----------------------------
+Attributes
+ Title: Aunts aren't Gentlemen
+ Author: PG Wodehouse
+
+ Behavior:
+   Get Description
+
+Create three objects that represent the three books shown above. 
+The method for the "Get Description" behavior should return a string like the following:
+
+"Me Talk Pretty One Day was written by David Sedaris."
+*/
+
+function createBook(title, author) {
+  return {
+    title: title,
+    author: author,
+
+    getDescription() {
+      console.log(`${this.title} was written by ${this.author}.`);
+    }
+  };
+}
+
+let book1 = createBook('Mythos', 'Stephen Fry');
+let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris');
+
+book1.getDescription();
+book2.getDescription();
+
