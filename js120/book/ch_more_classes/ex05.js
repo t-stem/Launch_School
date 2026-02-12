9c37c3b112e8ed1905bad39163cad8b940bde00f
Tim <N/A>
2026-02-12 13:11:26 +0000

    submitting js120 book ch more classes ex05

---
+++ js120/book/ch_more_classes/ex05.js
@@ -0,0 +1,32 @@
+/*
+Create a MathUtils class with static methods add, subtract, multiply, and divide. 
+These methods should perform basic arithmetic operations.
+*/
+
+class MathUtils {
+  static add(num1, num2) {
+    return num1 + num2;
+  }
+
+  static subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  static multiply(num1, num2) {
+    return num1 * num2;
+  }
+
+  static divide(num1, num2) {
+    if (num2 === 0) {
+      throw new RangeError('Division by zero');
+    } else {
+      return num1 / num2;
+    }
+  }
+}
+
+console.log(MathUtils.add(5, 3));       // 8
+console.log(MathUtils.subtract(10, 4)); // 6
+console.log(MathUtils.multiply(6, 7));  // 42
+console.log(MathUtils.divide(20, 5));   // 4
+console.log(MathUtils.divide(10, 0));   // RangeError: Division by zero
