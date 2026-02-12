7b8619b073d8df8ff824c57d7a45f0d5bb205dd4
Tim <N/A>
2026-02-12 10:08:55 +0000

    submitting js120 book ch classes ex03

---
+++ js120/book/ch_classes/ex03.js
@@ -0,0 +1,59 @@
+/*
+Create a class hierarchy consisting of vehicles, including cars, boats, and planes, as specific kinds of vehicles. 
+All vehicles should be able to accelerate and decelerate. 
+Cars should be able to honk, boats should be able to drop anchor, and planes should be able to take off and land. 
+Test your code.
+
+All vehicles should have a color and weight. Cars have a license number, boats have a home port, and planes have an airline name.
+*/
+
+class Vehicle {
+  constructor (color, weight) {
+    this.color = color;
+    this.weight = weight
+  }
+  
+  accelerate() {
+    console.log('Accelerating...');
+  }
+  decelerate() {
+    console.log('Decelerating...');
+  }
+}
+
+class Plane extends Vehicle {
+  constructor(color, weight, airlineName) {
+    super(color, weight);
+    this.airline = airlineName;
+  }
+
+  takeOff() {
+    console.log('Taking off...');
+  }
+
+  land() {
+    console.log('Landing...');
+  }
+}
+
+class Car extends Vehicle{
+  constructor(color, weight, licenseNumber) {
+    super(color, weight);
+    this.licenseNumber = licenseNumber;
+  }
+
+  honk() {
+    console.log('Honking...');
+  }
+}
+
+class Boat extends Vehicle {
+  constructor(color, weight, homePort) {
+  super(color, weight);
+  this.homePort = homePort;
+  }
+
+  dropAnchor() {
+    console.log('Dropping anchor...')
+  }
+}
