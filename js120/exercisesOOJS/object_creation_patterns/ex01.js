ab4ffc48ac4b2e3f4ae502e7c121861b44b2e1d6
Tim <N/A>
2026-02-13 17:10:59 +0000

    test

---
+++ js120/exercisesOOJS/object_creation_patterns/ex01.js
@@ -0,0 +1,39 @@
+/*
+Implement an ancestors method that returns the prototype chain (ancestors) of a calling object as an array of object names. 
+
+Here's an example output:
+*/
+
+
+// name property added to make objects easier to identify
+const foo = {name: 'foo'};
+const bar = Object.create(foo);
+bar.name = 'bar';
+const baz = Object.create(bar);
+baz.name = 'baz';
+const qux = Object.create(baz);
+qux.name = 'qux';
+
+Object.getPrototypeOf(foo).ancestors = function() {
+  let proto;
+  let nextObj = this;
+  let protosArray = []
+
+  do {
+    proto = Object.getPrototypeOf(nextObj);
+    
+    if (proto !== null) {
+      protosArray.push(proto.name);
+    }
+    
+    nextObj = proto;
+  }
+  while (proto !== null);
+
+  return protosArray;
+}
+
+console.log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
+console.log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
+console.log(bar.ancestors());  // returns ['foo', 'Object.prototype']
+console.log(foo.ancestors());  // returns ['Object.prototype']
